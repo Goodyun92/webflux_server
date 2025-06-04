ab28feea9d30f21564846eadfa2adbc88398674c
dongyun <0902hdy@gmail.com>
2025-06-05 01:20:54 +0900

    initial commit

---
+++ src/main/java/com/ls/webflux_server/WebfluxServerApplication.java
@@ -0,0 +1,13 @@
+package com.ls.webflux_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ls/webflux_server/WebfluxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ls.webflux_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
